59bdf9855b5e3dfadb0eb2a80ac3a9ea824b7599
MishaVatulich <57654804+MishaVatulich@users.noreply.github.com>
2020-02-18 17:14:19 +1000

    Add files via upload

---
+++ 1.1/main.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from Map_API import MapAPI
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        MapAPI(['140.232', '40.214'], '15', 'sat').draw()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap('map.png')
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
