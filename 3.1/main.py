4a3652c1f6894e3614f8d5cccac84f6d3a0513e5
kakashi-hatake3 <60177171+kakashi-hatake3@users.noreply.github.com>
2020-02-22 15:35:04 +1000

    Add files via upload

---
+++ 3.1/main.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from Map_API import MapAPI
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
+
+SCREEN_SIZE = [600, 450]
+
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.map_api = MapAPI()  # сама карта
+        self.map_api.draw()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap('map.png')
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and int(self.map_api.zoom) < 17:
+            self.map_api.zoom = str(int(self.map_api.zoom) + 1)
+            self.map_api.draw()
+            self.image.setPixmap(QPixmap('map.png'))
+        elif event.key() == Qt.Key_PageDown and int(self.map_api.zoom) > 0:
+            self.map_api.zoom = str(int(self.map_api.zoom) - 1)
+            self.map_api.draw()
+            self.image.setPixmap(QPixmap('map.png'))
+        elif event.key() == Qt.Key_Left and int(self.map_api.cords[0]) - 3 >= 0:
+            self.map_api.cords[0] = str(int(self.map_api.cords[0]) - 3)
+            self.map_api.draw()
+            self.image.setPixmap(QPixmap('map.png'))
+        elif event.key() == Qt.Key_Right and int(self.map_api.cords[0]) + 3 <= 180:
+            self.map_api.cords[0] = str(int(self.map_api.cords[0]) + 3)
+            self.map_api.draw()
+            self.image.setPixmap(QPixmap('map.png'))
+        elif event.key() == Qt.Key_Up and int(self.map_api.cords[1]) + 2 <= 90:
+            self.map_api.cords[1] = str(int(self.map_api.cords[1]) + 2)
+            self.map_api.draw()
+            self.image.setPixmap(QPixmap('map.png'))
+        elif event.key() == Qt.Key_Down and int(self.map_api.cords[1]) - 2 >= -90:
+            self.map_api.cords[1] = str(int(self.map_api.cords[1]) - 2)
+            self.map_api.draw()
+            self.image.setPixmap(QPixmap('map.png'))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
