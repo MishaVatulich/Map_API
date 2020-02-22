da2567a795e4847fa57d3de13b6654cb79a7868b
AragonerVL <50007349+AragonerVL@users.noreply.github.com>
2020-02-22 14:36:53 +1000

    Update afasg.py
    for button

--- 2.1/afasg.py
+++ 2.1/afasg.py
@@ -1,46 +1,15 @@
-import os
-import sys
-from Map_API import MapAPI
-
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtCore import Qt
-
-SCREEN_SIZE = [600, 450]
-
-
-class Example(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.coords = ['140.232', '40.214']
-        self.zoom = '15'
-        self.mod = 'sat'
-        MapAPI(self.coords, self.zoom, self.mod).draw()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap('map.png')
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
-
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_PgUp:
-            # self.zoom = str(int(self.zoom) + 1)
-            # MapAPI(self.coords, self.zoom, self.mod).draw()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+def keyPressEvent(self, event):
+    if event.key() == Qt.Key_PageUp and int(self.map_api.zoom) < 20:
+        self.map_api.zoom = str(int(self.map_api.zoom) + 1)
+        self.map_api.draw()
+    elif event.key() == Qt.Key_PageDown and int(self.map_api.zoom) > 0:
+        self.map_api.zoom = str(int(self.map_api.zoom) - 1)
+        self.map_api.draw()
+    elif event.key() == Qt.Key_Left:
+        pass
+    elif event.key() == Qt.Key_Right:
+        pass
+    elif event.key() == Qt.Key_Up:
+        pass
+    elif event.key() == Qt.Key_Down:
+        pass
