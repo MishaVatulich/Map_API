bc8f211789988102356e223dc883c67626852339
MishaVatulich <57654804+MishaVatulich@users.noreply.github.com>
2020-02-22 15:19:02 +1000

    Update Map_API.py
    работает переключение вида карты

--- 2.4/Map_API.py
+++ 2.4/Map_API.py
@@ -3,16 +3,22 @@ import requests
 
 class MapAPI():
     def __init__(self):
-        self.cords = ['140.232', '40.214']
+        self.cords = ['135.232', '45.214']
         self.zoom = '15'
-        self.mod = 'sat'
+        self.mod = 0  # вид карты
         self.api_server = "http://static-maps.yandex.ru/1.x/"
 
     def draw(self):  # обновляет файл с картой
+        if self.mod % 3 == 0:
+            map_kind = 'sat'
+        elif self.mod % 3 == 1:
+            map_kind = 'map'
+        elif self.mod % 3 == 2:
+            map_kind = 'skl'
         params = {
             "ll": ",".join([self.cords[0], self.cords[1]]),
             "z": self.zoom,
-            "l": self.mod
+            "l": map_kind
         }
         response = requests.get(self.api_server, params=params)
         # Запишем полученное изображение в файл.
